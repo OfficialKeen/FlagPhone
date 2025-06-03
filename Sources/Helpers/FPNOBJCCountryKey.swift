1ba96986aee1e3ed12b3cf00186a9e122f4297ee
Keen <zhekeen90@gmail.com>
2025-06-03 11:25:54 +0700

    Package

--- Sources/Helpers/FPNOBJCCountryKey.swift
+++ Sources/Helpers/FPNOBJCCountryKey.swift
@@ -489,7 +489,7 @@ import Foundation
 	FPNOBJCCountryKey.AX: "AX"
 ]*/
 
-public class FPNOBJCCountryCodeManager {
+public actor FPNOBJCCountryCodeManager {
     private var countryCode: [FPNOBJCCountryKey: String] = [
         .AF: "AF",
         .ZA: "ZA",
